a9e84b79f383310f1083b309dee080d9a7ecc07d
seidaiegoshi <egoshi.seidai@gmail.com>
2022-11-20 18:27:46 +0900

    ロード時にfirebaseにアクセス。データを取得してデータがあったら掲示板に表示する。#34

--- js/firebase.js
+++ js/firebase.js
@@ -25,34 +25,60 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+// 受信処理
+// URLを取得
+const url = new URL(window.location.href);
+
+// URLSearchParamsオブジェクトを取得
+const params = url.searchParams;
+
+// consoleに受け取ったパラメータを出力
+// console.log(params);
+
+// パラメータからラベル名を取得
+const label_name = params.get("sendLabel");
+// console.log(label_name);
+$("#board_name").text(label_name);
+
+// 新規か既存か分類
+// データがあるかないか
+const docRef = doc(db, "label", label_name); //ドキュメントの場所の変数docRefに入れる
+const docSnap = await getDoc(docRef); //docSnapにデータを入れる
+// console.log(docSnap); //docSnapにはよくわかんない情報が入ってるけど、.data()をつけるとわかりやすく見えるようになる
+
+let board = [];
+
+if (docSnap.exists()) {
+	console.log("Document data:", docSnap.data());
+	console.log(docSnap.data().board[0].user_name);
+
+	board = docSnap.data().board;
+	// board.push(board_comment);
+
+	board.forEach((element, i) => {
+		$("#post_contents").html(`
+		<li>
+			<div>${element.user_name}</div>
+			<div>${element.text}</div>
+		</li>
+`);
+	});
+
+	// F_board.push()
+} else {
+	// doc.data() will be undefined in this case
+	// コメントを登録する
+	console.log("No such document!");
+	$("#post_contents").html(`
+		<li>
+			<div>扉の番人</div>
+			<div>あなたが最初の発見者です。</div>
+		</li>
+`);
+}
+
 $("#date_came").on("click", async function () {
-	// labelを登録
-	const label_name = $("#label_name").val(); //label名を取得
-	console.log(label_name);
-	// label登録終了
-
-	// 新規か既存か分類
-	// データがあるかないか
-	const docRef = doc(db, "label", label_name); //ドキュメントの場所の変数docRefに入れる
-	const docSnap = await getDoc(docRef); //docSnapにデータを入れる
-	console.log(docSnap); //docSnapにはよくわかんない情報が入ってるけど、.data()をつけるとわかりやすく見えるようになる
-
-	let board = [];
-
-	if (docSnap.exists()) {
-		console.log("Document data:", docSnap.data());
-		console.log(docSnap.data().board[0].user_name);
-
-		board = docSnap.data().board;
-		// board.push(board_comment);
-
-		// F_board.push()
-	} else {
-		// doc.data() will be undefined in this case
-		// コメントを登録する
-		console.log("No such document!");
-	}
-
 	const user_name = $("#user_name").val();
 	const text = $("#text").val();
 
@@ -73,24 +99,8 @@ $("#date_came").on("click", async function () {
 	$("#text").val("");
 	// コメント登録ゾーン終了
 
+	//ドキュメントを更新する。
 	setDoc(doc(db, "label", label_name), {
 		board,
 	});
 });
-
-// 受信処理
-window.onload = function () {
-	// URLを取得
-	const url = new URL(window.location.href);
-
-	// URLSearchParamsオブジェクトを取得
-	const params = url.searchParams;
-
-	// consoleに受け取ったパラメータを出力
-	console.log(params);
-
-	// パラメータから「cameraData」を取得
-	const label = params.get("sendLabel");
-	console.log(label);
-	$("#label_name").val(label);
-};
